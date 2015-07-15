3d60ff12a73888820ef55c1ec4a18fd3bbfea850
willrees <william.rees@bondbrandloyalty.com>
2015-07-15 13:56:00 -0400

    Used global component factory to define the http functions.

--- scripts/src/dm.http.js
+++ scripts/src/dm.http.js
@@ -1,51 +1,12 @@
-window.dm.http = function ($, configuration) {
-
+dm.globalComponentFactory("http", function() {
+    var config = dm.config;
+    
     var settings = {
         debug: false,
         validateAntiForgeryToken: false,
         redirectOnAllErrors: false                
     };
-
-    var send = function (options) {
-        areSettingsValid();
-        var deferred = $.Deferred();
-
-        options.success = function (response, status, xhr) {
-            if (response.Success || (response.Success === undefined && xhr.status === 200)) {
-                deferred.resolve(response);
-            } else if (settings.redirectOnAllErrors === true) {
-                redirectToErrorView(xhr);
-            } else {
-                deferred.reject(response.Errors);
-            }
-        };
-
-        options.error = function (d, textStatus, error) {
-            redirectToErrorView(d, textStatus, error);
-        };
-        
-        if (options.data && settings.validateAntiForgeryToken === true) {
-            options.data.__RequestVerificationToken = $('input[name="__RequestVerificationToken"]').val();
-        }
-
-        $.ajax(options);
-
-        return deferred.promise();
-    };    
-
-    var redirectToErrorView = function (d) {
-        if ((d.status === 450 || d.status === 401 || d.status === 403) && configuration.authentication.usesAuthentication) {
-            window.location = settings.loginPage + "?returnUrl=" + window.location.href.replace(settings.basePath, "/");
-        } else if (settings.debug === false && configuration.errors.redirectOnErrors) {
-            window.location = settings.errorPage;
-        } else {
-            if (settings.debug) {
-                alert("HTTP error occured");
-                console.log(arguments);    
-            }
-        }
-    };
-
+    
     var _get = function (url) {
         return send({
             type: "GET",
@@ -81,21 +42,61 @@ window.dm.http = function ($, configuration) {
         });
     };
     
-    function areSettingsValid() {
-        if (configuration.errors.redirectOnErrors && configuration.errors.defaultErrorPage === undefined) {
-            throw "You must set the path to the sites generic error page in dm.configuration.errors.defaultErrorPage";    
+    function send (options) {
+        areSettingsValid();
+        var deferred = $.Deferred();
+
+        options.success = function (response, status, xhr) {
+            if (response.Success || (response.Success === undefined && xhr.status === 200)) {
+                deferred.resolve(response);
+            } else if (settings.redirectOnAllErrors === true) {
+                redirectToErrorView(xhr);
+            } else {
+                deferred.reject(response.Errors);
+            }
+        };
+
+        options.error = function (d, textStatus, error) {
+            redirectToErrorView(d, textStatus, error);
+        };
+        
+        if (options.data && settings.validateAntiForgeryToken === true) {
+            options.data.__RequestVerificationToken = $('input[name="__RequestVerificationToken"]').val();
         }
-        
-        if (configuration.authentication.usesAuthentication && configuration.authentication.loginPage === undefined) {
-            throw "You must set the path to the sites login page in dm.authentication.loginPage";    
-        }        
-        
-        if (configuration.basePath !== undefined)
-        {
-            throw "The sites base path has not been configured in dm.configuration.basePath";
+
+        $.ajax(options);
+
+        return deferred.promise();
+    }
+    
+    function redirectToErrorView (d) {
+        if ((d.status === 450 || d.status === 401 || d.status === 403) && config.http.authentication.usesAuthentication) {
+            window.location = settings.loginPage + "?returnUrl=" + window.location.href.replace(settings.basePath, "/");
+        } else if (settings.debug === false && config.http.errors.redirectOnErrors) {
+            window.location = settings.errorPage;
+        } else {
+            if (settings.debug) {
+                alert("HTTP error occured");
+                console.log(arguments);    
+            }
         }
     }
-
+    
+    function areSettingsValid() {
+        if (config.http.errors.redirectOnErrors && config.http.errors.defaultErrorPage === undefined) {
+            throw "You must set the path to the sites generic error page in dm.config.http.errors.defaultErrorPage";    
+        }
+        
+        if (config.http.authentication.usesAuthentication && config.http.authentication.loginPage === undefined) {
+            throw "You must set the path to the sites login page in dm.config.http.authentication.loginPage";    
+        }        
+        
+        if (config.basePath !== undefined)
+        {
+            throw "The sites base path has not been configured in dm.config.basePath";
+        }
+    }
+    
     return {
         get: _get,
         post: _post,
@@ -103,4 +104,15 @@ window.dm.http = function ($, configuration) {
         delete: _delete,
         settings: settings
     };
-}(jQuery, dm.config);
+     
+}, 
+{
+    errors: {
+		redirectOnErrors: true,
+		defaultErrorPage: undefined	
+	},
+	authentication: {
+		usesAuthentication: true,
+		loginPage: undefined
+	}
+}, [jQuery]);
