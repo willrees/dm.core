8e56a8311ce2103a948598088d0e1dd43a136e19
willrees <william.rees@bondbrandloyalty.com>
2015-07-15 14:37:24 -0400

    Wrapped dm.List in the create global component function.

--- scripts/src/dm.List.js
+++ scripts/src/dm.List.js
@@ -1,79 +1,79 @@
-window.dm.List = function (array) {
-	"use strict";
-	var arr = [];
-	
-	if (array !== undefined) {
-		arr = array;
-	}
-	
-	arr.first = function () {
-		return this[0];	
-	};
-	
-	arr.last = function () {
-		return this[this.length - 1];	
-	};
-	
-	arr.where = function (test) {
-		if (typeof test != "function")
-		{
-			throw new TypeError();
+dm.globalComponentFactory("List", function(){
+	return function(array) {
+		var arr = [];
+		
+		if (array !== undefined) {
+			arr = array;
 		}
 		
-		var whereResults = [];
-		arr.forEach(function (item, index, array) {
-			if (test(JSON.parse(JSON.stringify(item)))) {
-				whereResults.push(item);
+		arr.first = function () {
+			return this[0];	
+		};
+		
+		arr.last = function () {
+			return this[this.length - 1];	
+		};
+		
+		arr.where = function (test) {
+			if (typeof test != "function")
+			{
+				throw new TypeError();
 			}
- 		});
+			
+			var whereResults = [];
+			arr.forEach(function (item, index, array) {
+				if (test(JSON.parse(JSON.stringify(item)))) {
+					whereResults.push(item);
+				}
+	 		});
+			
+			return new dm.List(whereResults);
+		};
 		
-		return new dm.List(whereResults);
-	};
-	
-	arr.pluck = function (propertyName) {
-		if (typeof propertyName != "string") 
-		{
-			throw new TypeError();
-		}
-		
-		var pluckResults = [];
-		arr.forEach(function (item, index, array) {
-			if (item[propertyName] !== undefined) {
-				pluckResults.push(item[propertyName]);
+		arr.pluck = function (propertyName) {
+			if (typeof propertyName != "string") 
+			{
+				throw new TypeError();
 			}
-		});
+			
+			var pluckResults = [];
+			arr.forEach(function (item, index, array) {
+				if (item[propertyName] !== undefined) {
+					pluckResults.push(item[propertyName]);
+				}
+			});
+			
+			return new dm.List(pluckResults);
+		};
 		
-		return new dm.List(pluckResults);
-	};
+		arr.shuffle = function() {
+		  var currentIndex = arr.length;
+		  var temporaryValue;
+		  var randomIndex ;
+		
+		  while (0 !== currentIndex) {
 	
-	arr.shuffle = function() {
-	  var currentIndex = arr.length;
-	  var temporaryValue;
-	  var randomIndex ;
-	
-	  while (0 !== currentIndex) {
-
-	    randomIndex = Math.floor(Math.random() * currentIndex);
-	    currentIndex -= 1;
-	
-	    temporaryValue = arr[currentIndex];
-	    arr[currentIndex] = arr[randomIndex];
-	    arr[randomIndex] = temporaryValue;
-	  }
-	
-	  return arr;
-	};
-	
-	//IE8 Polyfills
-	  if (!arr["forEach"]) {
+		    randomIndex = Math.floor(Math.random() * currentIndex);
+		    currentIndex -= 1;
+		
+		    temporaryValue = arr[currentIndex];
+		    arr[currentIndex] = arr[randomIndex];
+		    arr[randomIndex] = temporaryValue;
+		  }
+		
+		  return arr;
+		};
+		
+		//IE8 Polyfills
+		if (!arr["forEach"]) {
 		  arr.forEach = function (callback) {
 			  for (var i=0; i < this.length; i++) {
 				  callback.apply(this, [this[i], i, this]);
 			  }
 		  };
-	  }
-	  
-	  if (!arr["indexOf"]) {
+		}
+		  
+		if (!arr["indexOf"]) {
 		  arr.indexOf = function (obj, start) {
 			  for (var i = (start || 0), j = this.length; i < j; i++) {
 				  if (this[i] === obj) {
@@ -81,7 +81,9 @@ window.dm.List = function (array) {
 				  }
 			  }
 		  };
-	  }
+		}
 	
-	return arr;	
-};
+		return arr;	
+		
+	}
+}, null, []);
